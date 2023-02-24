3c890322ac29bf732a550041d5c12b86c5e049c9
Malfan1 <artemdid911@gmail.com>
2023-02-24 03:14:07 +0200

    finish

--- js/pop.js
+++ js/pop.js
@@ -1,3 +1,9 @@
 $(".menu-burder").click(function(){
     $(".nav").addClass("open");
+    $(".visible").addClass("open-vis");
+});
+
+$(".visible").click(function(){
+    $(".nav").removeClass("open");
+    $(".visible").removeClass("open-vis");
 });
